a0ba0cfda8f6e9c05c6b59ce3a66094289c3816b
Andre <andreluiz.miranda@outlook.com>
2026-02-01 17:13:38 -0300

    adicionado projeto BFF

---
+++ src/main/java/com/javanauta/bffagendadordetarefas/BffAgendadorDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.javanauta.bffagendadordetarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffAgendadorDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffAgendadorDeTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javanauta/bffagendadordetarefas/BffAgendadorDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanauta.bffagendadordetarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
